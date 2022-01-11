1e6bf55efc4ffbcdfcae166e555947cbae3629cc
REACH47 <68965190+REACH47@users.noreply.github.com>
2022-01-11 02:30:37 -0500

    added basic styling to header

--- airfrythis/src/Components/Header/Header.jsx
+++ airfrythis/src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Header.scss";
 
 function Header(props) {
   return (
     <>
-      <h1>{props.title}</h1>
+      <h1 className="header-title">{props.title}</h1>
     </>
   );
 }
