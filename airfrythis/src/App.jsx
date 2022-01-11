feeed3ca51f87716b50e2a5bedbb77f9028d4b23
REACH47 <68965190+REACH47@users.noreply.github.com>
2022-01-11 02:48:19 -0500

    added Assets folder, header font. Created typography partial and connected to header component. Adjusted header component with style/font as accordingly

--- airfrythis/src/App.jsx
+++ airfrythis/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./Components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <Header title="Air Fry This" />
+      <Header title="AIR FRY THIS" />
     </div>
   );
 }
