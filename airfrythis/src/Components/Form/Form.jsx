b3177ccb6607662ebd3fe3ce9a00d38cf32c4cec
REACH47 <68965190+REACH47@users.noreply.github.com>
2022-01-12 02:34:10 -0500

    added form temporary form styling

--- airfrythis/src/Components/Form/Form.jsx
+++ airfrythis/src/Components/Form/Form.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import "./Form.scss";
 
 function Form() {
   const handleOnSubmit = (e) => {
     e.preventDefault();
   };
+
   return (
-    <div>
-      <form action="submit">
-        <input type="text" placeholder="This is a input" />
-        <input type="text" placeholder="This is a input" />
-        <input type="text" placeholder="This is a input" />
+    <div className="form">
+      <form className="form__container" action="submit">
+        <input
+          className="form__input"
+          type="text"
+          placeholder="This is a input"
+        />
+        <input
+          className="form__input"
+          type="text"
+          placeholder="This is a input"
+        />
+        <input
+          className="form__input"
+          type="text"
+          placeholder="This is a input"
+        />
       </form>
     </div>
   );
