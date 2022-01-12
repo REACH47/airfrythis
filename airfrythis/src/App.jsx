e89741ee3ba9efa6c422e01e568ed097233e48a2
REACH47 <68965190+REACH47@users.noreply.github.com>
2022-01-12 02:19:58 -0500

    created form components and added placeholder input to form

--- airfrythis/src/App.jsx
+++ airfrythis/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header/Header";
 import FryerCard from "./Components/FryerCard/FryerCard";
+import Form from "./Components/Form/Form";
 import "./styles/global.scss";
 
 function App() {
   return (
     <div className="App">
       <Header title="AIR FRY THIS" />
+      <Form />
       <FryerCard />
     </div>
   );
---
+++ airfrythis/src/Components/Form/Form.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <form action="submit">
+        <input type="text" placeholder="This is a input" />
+        <input type="text" placeholder="This is a input" />
+        <input type="text" placeholder="This is a input" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
