de315e4f94d740aa3a4a30883758abcf6d8a0d40
REACH47 <68965190+REACH47@users.noreply.github.com>
2022-01-13 02:28:24 -0500

    linked welcome button to form page

--- airfrythis/src/Components/Welcome/Welcome.jsx
+++ airfrythis/src/Components/Welcome/Welcome.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Welcome.scss";
 
 function Welcome() {
@@ -7,7 +8,9 @@ function Welcome() {
       <div className="welcome">
         <h1 className="welcome__title">AIR FRY THIS</h1>
         <div className="welcome__enter-container">
-          <button className="welcome__enter">start frying</button>
+          <Link to="/form">
+            <button className="welcome__enter">start frying</button>
+          </Link>
         </div>
       </div>
     </>
