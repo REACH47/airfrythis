86f59ade1596efc33632d3bf3e87a5de3025b4b1
REACH47 <68965190+REACH47@users.noreply.github.com>
2022-01-13 01:43:51 -0500

    added landing page component

---
+++ airfrythis/src/Components/Welcome/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Welcome() {
+  return (
+    <>
+      <div className="welcome">
+        <h1 className="welcome__container">AIR FRY THIS</h1>
+        <button className="welcome__enter">start frying</button>
+      </div>
+    </>
+  );
+}
+
+export default Welcome;
